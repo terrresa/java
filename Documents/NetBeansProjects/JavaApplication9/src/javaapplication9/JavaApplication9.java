6d89c234f722262c3a6df2090dca8d56e9ef1901
terrresa <eleana_marchan@hotmail.com>
2015-04-09 18:20:24 -0500

    Update JavaApplication9.java

--- Documents/NetBeansProjects/JavaApplication9/src/javaapplication9/JavaApplication9.java
+++ Documents/NetBeansProjects/JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -16,7 +16,7 @@ public class JavaApplication9 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println(" probando ");
+        System.out.println(" probando  desde github hacia netbeans");
     }
     
 }
